29bee786646b9528ef90b8408904e6a5f865d467
Tushar Diwakar <www.tushardiwakar@gmail.com>
2022-02-11 23:20:48 +0530

    handling errors for authentication

--- dist/resolvers/user.js
+++ dist/resolvers/user.js
@@ -60,10 +60,40 @@ UserResponse = __decorate([
 ], UserResponse);
 let UserResolver = class UserResolver {
     async register(options, { em }) {
+        if (options.username.length <= 2) {
+            return {
+                errors: [{
+                        field: "username",
+                        message: "length lesser than 2"
+                    }]
+            };
+        }
+        if (options.password.length <= 3) {
+            return {
+                errors: [{
+                        field: "password",
+                        message: "length lesser than 3"
+                    }]
+            };
+        }
         const hashedPassword = await argon2_1.default.hash(options.password);
         const user = em.create(User_1.User, { username: options.username, password: hashedPassword });
-        await em.persistAndFlush(user);
-        return user;
+        try {
+            await em.persistAndFlush(user);
+        }
+        catch (err) {
+            if (err.code == "23505") {
+                return {
+                    errors: [{
+                            field: "username",
+                            message: "username already taken",
+                        }]
+                };
+            }
+        }
+        return {
+            user,
+        };
     }
     async login(options, { em }) {
         const user = await em.findOne(User_1.User, { username: options.username });
@@ -92,7 +122,7 @@ let UserResolver = class UserResolver {
     }
 };
 __decorate([
-    (0, type_graphql_1.Mutation)(() => User_1.User),
+    (0, type_graphql_1.Mutation)(() => UserResponse),
     __param(0, (0, type_graphql_1.Arg)('options')),
     __param(1, (0, type_graphql_1.Ctx)()),
     __metadata("design:type", Function),
